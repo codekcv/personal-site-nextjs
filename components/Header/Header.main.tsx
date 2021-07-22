901afed6aed2dad44487aa3b886bac8ed29a261f
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-22 19:25:21 +0800

    feat: header menu init

--- components/Header/Header.main.tsx
+++ components/Header/Header.main.tsx
@@ -1,13 +1,26 @@
-import Box from 'components/general/Box/Box.main'
+import Flex from 'components/general/Flex/Flex.main'
+import Item from 'components/general/Item/Item.main'
 import Text from 'components/general/Text/Text.main'
 import styled from 'styled-components'
 
-const Container = styled(Box)``
+const Container = styled(Flex)`
+  li {
+    list-style: none;
+  }
+`
 
-const Header = (): JSX.Element => (
-  <Container width={1} mt="2rem">
-    <Text textAlign="center">&nbsp;</Text>
-  </Container>
-)
+const menus = ['Home', 'Projects', 'Blogs', 'Contact'].map((menu) => (
+  <Item key={menu} as="li" mx={2}>
+    <Text>{menu}</Text>
+  </Item>
+))
+
+const Header = (): JSX.Element => {
+  return (
+    <Container width={1} my="2rem" justifyContent="center" alignItems="center">
+      <Flex as="ul">{menus}</Flex>
+    </Container>
+  )
+}
 
 export default Header
