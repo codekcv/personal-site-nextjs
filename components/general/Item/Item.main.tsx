cdec0fd76cf5ef266f19b507936a8fb2bff36fe1
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-22 19:24:36 +0800

    feat: item component init

---
+++ components/general/Item/Item.main.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+import { compose, layout, LayoutProps, space, SpaceProps } from 'styled-system'
+
+type Props = LayoutProps & SpaceProps
+
+const Item = styled.li<Props>`
+  ${compose(layout, space)}
+`
+
+export default Item
