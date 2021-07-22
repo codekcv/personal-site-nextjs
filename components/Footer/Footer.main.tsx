7f21c8efe31005757eaa5474a65af50565a9b75d
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-22 18:44:12 +0800

    feat: footer update

--- components/Footer/Footer.main.tsx
+++ components/Footer/Footer.main.tsx
@@ -1,12 +1,19 @@
 import Box from 'components/general/Box/Box.main'
 import Text from 'components/general/Text/Text.main'
 
-const Footer = (): JSX.Element => (
-  <Box width={1} mt="2rem">
-    <Text textAlign="center">
-      © {new Date().getFullYear()} Christian Villamin
-    </Text>
-  </Box>
-)
+const Footer = (): JSX.Element => {
+  const currentYear = new Date().getFullYear()
+
+  return (
+    <Box width={1} mt="2rem">
+      <Text textAlign="center">
+        © {currentYear} Christian Villamin | Built with&nbsp;
+        <a href="https://nextjs.org/" target="_blank" rel="noopener noreferrer">
+          Next.js
+        </a>
+      </Text>
+    </Box>
+  )
+}
 
 export default Footer
