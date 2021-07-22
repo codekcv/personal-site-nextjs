77e779745983b0e1471820308178d5c3fe73b5c7
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-22 18:03:26 +0800

    feat: convrt to arrow fn

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (
+  <Component {...pageProps} />
+)
+
 export default MyApp
