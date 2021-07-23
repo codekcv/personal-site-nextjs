0a90433fe3a148f7a26d05508431c88af1de26e9
Christian Villamin <ChristianVillamin31@gmail.com>
2021-07-23 10:09:33 +0800

    feat: card css

--- components/Card/Card.main.tsx
+++ components/Card/Card.main.tsx
@@ -4,8 +4,11 @@ import { compose, layout, LayoutProps, space, SpaceProps } from 'styled-system'
 
 const Card = styled(motion.div)<LayoutProps & SpaceProps>`
   padding: 2rem;
+  border: 1px dotted rgba(0, 0, 0, 0.1);
   border-radius: 12px;
-  box-shadow: 0 0 3rem -1.5rem #ccc;
+  box-shadow: 0 0.75rem 3rem 0em rgba(0, 0, 0, 0.05),
+    0 0.75rem 0rem 0rem rgba(0, 0, 0, 0.05);
+
   ${compose(layout, space)}
 `
 
